61660e099d418aa74c53884866c1c98a58aa41db
jredfox <jjredfox@hotmail.com>
2019-02-12 17:55:09 -0600

    Cleanup XIX

--- src/main/java/com/evilnotch/menulib/event/MenuMusicEvent.java
+++ src/main/java/com/evilnotch/menulib/event/MenuMusicEvent.java
@@ -2,10 +2,11 @@ package com.evilnotch.menulib.event;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
 /**
- * use this event to stop vanilla music from playing when it's not your gui menu
+ * used to determine if music can play. Currently only supports vanilla however modders can fire this for their main menu music
  * @author jredfox
  */
 public class MenuMusicEvent extends Event {
@@ -17,6 +18,16 @@ public class MenuMusicEvent extends Event {
 	{
 		this.gui = screen;
 		this.canPlay = this.gui instanceof GuiMainMenu;
+	}
+	
+	/**
+	 * use this to fire the event
+	 */
+	public static MenuMusicEvent fireMusicEvent(GuiScreen screen)
+	{
+		MenuMusicEvent e = new MenuMusicEvent(screen);
+		MinecraftForge.EVENT_BUS.post(e);
+		return e;
 	}
 
 }
