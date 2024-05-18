4b45eb7bb5d95695ab49dd29881bc89fe1ce8628
jredfox <jredfox_@outlook.com>
2024-05-18 02:09:38 -0500

    stop steve

--- src/main/java/com/evilnotch/lib/main/eventhandler/ClientEvents.java
+++ src/main/java/com/evilnotch/lib/main/eventhandler/ClientEvents.java
@@ -2,39 +2,87 @@ package com.evilnotch.lib.main.eventhandler;
 
 import java.util.List;
 
-import com.evilnotch.lib.api.ReflectionUtil;
-import com.evilnotch.lib.api.mcp.MCPSidedString;
 import com.evilnotch.lib.main.Config;
-import com.evilnotch.lib.main.MainJava;
-import com.evilnotch.lib.main.skin.SkinCache;
 import com.evilnotch.lib.minecraft.basicmc.auto.json.JsonGen;
 import com.evilnotch.lib.minecraft.basicmc.client.gui.GuiBasicButton;
 import com.evilnotch.lib.minecraft.client.Seeds;
 import com.evilnotch.lib.minecraft.event.client.ClientDisconnectEvent;
 import com.evilnotch.lib.minecraft.event.client.SkinTransparencyEvent;
-import com.evilnotch.lib.minecraft.network.packet.PacketUUID;
 import com.evilnotch.lib.minecraft.proxy.ClientProxy;
 import com.evilnotch.lib.minecraft.tick.TickRegistry;
 import com.evilnotch.lib.minecraft.util.PlayerUtil;
-import com.mojang.authlib.properties.PropertyMap;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiDisconnected;
 import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.util.Session;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import net.minecraftforge.client.event.RenderHandEvent;
+import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 public class ClientEvents {
+	
+    public static boolean hasJoined2 = false;
+	@SubscribeEvent(priority=EventPriority.HIGH)
+	public void stopSteve(RenderPlayerEvent.Pre event)
+	{
+		if(hasJoined2)
+			return;
+		
+		if(event.getEntityPlayer() == Minecraft.getMinecraft().player)
+		{
+			EntityPlayerSP player = Minecraft.getMinecraft().player;
+			NetworkPlayerInfo info = player.connection.getPlayerInfo(player.getUniqueID());
+			if(info == null || info.skinType == null)
+			{
+				if(info != null)
+					info.getLocationSkin();//make it download the skin
+				event.setCanceled(true);
+			}
+			else
+			{
+				hasJoined2 = true;
+			}
+		}
+	}
+    
+	public static boolean hasJoined = false;
+	@SubscribeEvent(priority=EventPriority.HIGH)
+	public void stopSteve(RenderHandEvent event)
+	{
+		if(hasJoined)
+			return;
+		
+		//return from canceling if we are not inside of the world
+		Minecraft mc = Minecraft.getMinecraft();
+		if(mc.getRenderViewEntity() != mc.player || mc.world == null || mc.player == null)
+		{
+			return;
+		}
+		NetworkPlayerInfo info = mc.player.connection.getPlayerInfo(mc.player.getUniqueID());
+		if(info == null || info.skinType == null)
+		{
+			if(info != null)
+				info.getLocationSkin();//make it download the skin
+			event.setCanceled(true);
+		}
+		else
+		{
+			hasJoined = true;
+		}
+	}
 	
 	/**
 	 * future:Generate models with textures coming from the registry name
@@ -90,6 +138,8 @@ public class ClientEvents {
 	public void disconnect(ClientDisconnectEvent e)
 	{ 
 		ClientProxy.disconnect();
+		hasJoined = false;
+		hasJoined2 = false;
 	}
 	
 	@SubscribeEvent(priority = EventPriority.LOWEST)
