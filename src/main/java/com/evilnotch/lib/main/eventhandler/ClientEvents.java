e9692cfaaafe441345584f98828c6ffd6bd54f1a
jredfox <jredfox_@outlook.com>
2024-05-27 12:15:26 -0500

    Fix IItemRenderer Crashes

--- src/main/java/com/evilnotch/lib/main/eventhandler/ClientEvents.java
+++ src/main/java/com/evilnotch/lib/main/eventhandler/ClientEvents.java
@@ -3,9 +3,6 @@ package com.evilnotch.lib.main.eventhandler;
 import java.util.List;
 
 import com.evilnotch.lib.main.Config;
-import com.evilnotch.lib.main.skin.SkinCache;
-import com.evilnotch.lib.main.skin.SkinEntry;
-import com.evilnotch.lib.main.skin.SkinEvent;
 import com.evilnotch.lib.minecraft.basicmc.auto.json.JsonGen;
 import com.evilnotch.lib.minecraft.basicmc.client.gui.GuiBasicButton;
 import com.evilnotch.lib.minecraft.client.Seeds;
@@ -14,26 +11,18 @@ import com.evilnotch.lib.minecraft.event.client.SkinTransparencyEvent;
 import com.evilnotch.lib.minecraft.proxy.ClientProxy;
 import com.evilnotch.lib.minecraft.tick.TickRegistry;
 import com.evilnotch.lib.minecraft.util.PlayerUtil;
-import com.evilnotch.lib.util.JavaUtil;
-import com.mojang.authlib.properties.Property;
-import com.mojang.authlib.properties.PropertyMap;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiDisconnected;
 import net.minecraft.client.gui.GuiMainMenu;
-import net.minecraft.client.network.NetworkPlayerInfo;
-import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
-import net.minecraftforge.client.event.RenderHandEvent;
-import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -123,6 +112,16 @@ public class ClientEvents {
 	public void coolskins(SkinTransparencyEvent event)
 	{
 		event.allowTrans = Config.allowskintrans;
+	}
+	
+	/**
+	 * Vanilla Bug Fix Prevents Crashing When Rendering Entities Sooner then expected with nametags
+	 */
+	@SubscribeEvent
+	public void nametag(RenderLivingEvent.Specials.Pre event)
+	{
+		if(event.getRenderer().getRenderManager().renderViewEntity == null)
+			event.setCanceled(true);
 	}
 	
 }
