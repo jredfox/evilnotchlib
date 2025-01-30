dbc42331c972fccf9187ee15cb3b405257582fa9
jredfox <jredfox_@outlook.com>
2025-01-29 22:35:31 -0600

    Update SkinEvent.java

--- src/main/java/com/evilnotch/lib/main/skin/SkinEvent.java
+++ src/main/java/com/evilnotch/lib/main/skin/SkinEvent.java
@@ -50,12 +50,6 @@ public class SkinEvent extends Event {
 				this.skin.model = isAlex ? "slim" : "";
 			}
 			
-			//Debug Test
-//			this.skin.skin = "";
-//			this.skin.skin = "http://textures.minecraft.net/texture/$null";
-//			this.skin.skin = "http://textures.minecraft.net/texture/$alex";
-//			this.skin.model = "slim";
-			
 			SkinCache.setEncode(this.profile.getProperties(), this.skin.encode());
 		}
 	}
