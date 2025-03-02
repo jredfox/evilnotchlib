369a91cb1bc5eaf5109db8d65e858ef12915c922
jredfox <jredfox_@outlook.com>
2025-03-01 22:39:40 -0600

    Enchanted Capes WIP

---
+++ src/main/java/com/evilnotch/lib/minecraft/client/CapeRenderer.java
@@ -0,0 +1,65 @@
+package com.evilnotch.lib.minecraft.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.util.ResourceLocation;
+
+public class CapeRenderer {
+	
+	public static final ResourceLocation ENCHANTED_ITEM_GLINT_RES = new ResourceLocation("textures/misc/enchanted_item_glint.png");
+   
+	public static void render(RenderPlayer renderer, EntityLivingBase e, float partialTicks, float scale)
+    {
+		if(e.getItemStackFromSlot(EntityEquipmentSlot.CHEST).isItemEnchanted())
+			renderEnchantedCape(renderer, e, renderer.getMainModel(), partialTicks, scale);
+    }
+	
+	/**
+	 * A direct call to render an enchanted cape without checks. Use this for special logic
+	 * NOTE: 0.0625F is the default scale but mods could change the default via ASM scale just be sure to match it inside the LayerCape's class
+	 */
+	public static void renderEnchantedCape(RenderPlayer renderer, EntityLivingBase e, ModelPlayer model, float partialTicks, float scale)
+    {
+        float f = (float)e.ticksExisted + partialTicks;
+        renderer.bindTexture(ENCHANTED_ITEM_GLINT_RES);
+        Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
+        GlStateManager.enableBlend();
+        GlStateManager.depthFunc(514);
+        GlStateManager.depthMask(false);
+        float f1 = 0.5F;
+        GlStateManager.color(0.5F, 0.5F, 0.5F, 1.0F);
+
+        for (int i = 0; i < 2; ++i)
+        {
+            GlStateManager.disableLighting();
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_COLOR, GlStateManager.DestFactor.ONE);
+            float f2 = 0.76F;
+            GlStateManager.color(0.38F, 0.19F, 0.608F, 1.0F);
+            GlStateManager.matrixMode(5890);
+            GlStateManager.loadIdentity();
+            float f3 = 0.33333334F;
+            GlStateManager.scale(0.33333334F, 0.33333334F, 0.33333334F);
+            GlStateManager.rotate(30.0F - (float)i * 60.0F, 0.0F, 0.0F, 1.0F);
+            GlStateManager.translate(0.0F, f * (0.001F + (float)i * 0.003F) * 20.0F, 0.0F);
+            GlStateManager.matrixMode(5888);
+            model.renderCape(scale);
+            GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        }
+
+        GlStateManager.matrixMode(5890);
+        GlStateManager.loadIdentity();
+        GlStateManager.matrixMode(5888);
+        GlStateManager.enableLighting();
+        GlStateManager.depthMask(true);
+        GlStateManager.depthFunc(515);
+        GlStateManager.disableBlend();
+        Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
+    }
+
+}
